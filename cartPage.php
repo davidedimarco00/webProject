b5bf0c621e18ebfb9837d6890bf3e774bbcd257a
Riccardo <riccardo@hpavlion-r.homenet.telecomitalia.it>
2022-01-08 12:37:38 +0100

    base cart (php)

--- cartPage.php
+++ cartPage.php
@@ -2,17 +2,20 @@
 require_once 'bootstrap.php';
 
 //Placeholder Templates: da sostituire con le query al database
-$templateParams["css"] = "css/productPage.css";
-$templateParams["titolo"] = "Product Details";
-$templateParams["pagereq"] = "template/productpage.php";
+$templateParams["css"] = "css/cartPage.css";
+$templateParams["titolo"] = "Cart Details";
+$templateParams["pagereq"] = "template/cart.php";
 //$templateParams["nomearticolo"] = $dbh->metodo per prendere l'articolo richiesto;
 //Home Template
 //$templateParams["venditore"] = $dbh->metodo per ottenere il venditore;
+/*
 $templateParams["nomearticolo"] = "MIC XEN02";
 $templateParams["spedizione"] = "0";
 $templateParams["venditore"] = "briglia";
 $templateParams["prezzo"] = "50";
 $templateParams["caratteristiche"] = array(array("numero di porte", "8"), array("Potenza", "8Wh"), array("Dimensioni", "15cm x 15cm x 6cm"));
 $templateParams["immagini"] = array("images/mixer.jpg", "images/mixer2.jpg", "images/mixer3.jpg", "images/mixer.jpg", "images/mixer2.jpg", "images/mixer3.jpg");
+*/
 require 'template/base.php';
+
 ?>
