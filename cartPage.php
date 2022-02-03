a0c3b31c846f0c09fd8e682a38e59e94cc8c161c
Riccardo <riccardo@hpavlion-r.homenet.telecomitalia.it>
2022-02-03 17:25:08 +0100

    fix cart

--- cartPage.php
+++ cartPage.php
@@ -1,18 +1,24 @@
 <?php
 require_once 'bootstrap.php';
+if(!isUserLoggedIn()){
+    header("Location: loginPage.php");
+}
+if (isset($_GET["delete"])){
+    
+}
 //Placeholder Templates: da sostituire con le query al database
-$templateParams["css"] = array("css/cartPage.css", "css/header.css", "css/footer.css");
+$templateParams["css"] = array( "css/header.css", "css/footer.css","css/cartPage.css");
 $templateParams["titolo"] = "Cart Details";
 $templateParams["pagereq"] = "template/cartTemplate.php";
 
-$templateParams["items"] = array(array("name" => "MIXER XEN02", "vendor" => "briglia", "price" => "50", "images" => array("images/mixer.jpg", "images/mixer2.jpg", "images/mixer3.jpg")),
-array("name" => "Blue Yeti Microphone", "vendor" => "briglia", "price" => "132", "images" => array("https://m.media-amazon.com/images/I/61LmU6eBjyL._AC_SL1500_.jpg", "https://m.media-amazon.com/images/I/81bYEr6onaL._AC_SL1500_.jpg")),
-array("name" => "Anker SoundCore Life Q30", "vendor" => "briglia", "price" => "70", "images" => array("https://m.media-amazon.com/images/I/61+WYAjltpL._AC_SL1500_.jpg", "https://m.media-amazon.com/images/I/71td6PxaFNL._AC_SL1500_.jpg")));
+$templateParams["items"] = array(array("name" => "MIXER XEN02", "vendor" => "briglia", "price" => "50", "CodProdotto" => "1345", "images" => array("images/mixer.jpg", "images/mixer2.jpg", "images/mixer3.jpg")),
+array("name" => "Blue Yeti Microphone", "vendor" => "briglia", "price" => "132","CodProdotto" => "2145", "images" => array("https://m.media-amazon.com/images/I/61LmU6eBjyL._AC_SL1500_.jpg", "https://m.media-amazon.com/images/I/81bYEr6onaL._AC_SL1500_.jpg")),
+array("name" => "Anker SoundCore Life Q30", "vendor" => "briglia", "price" => "70","CodProdotto" => "2896", "images" => array("https://m.media-amazon.com/images/I/61+WYAjltpL._AC_SL1500_.jpg", "https://m.media-amazon.com/images/I/71td6PxaFNL._AC_SL1500_.jpg")));
 
-
+//$templateParams["items"] = $dbh->
 $templateParams["total"] = 0;
 foreach($templateParams["items"] as $item):
-    $templateParams["total"]+=$item["price"];
+    $templateParams["total"]+=$item["price"]; 
 endforeach;
 
 
--- template/cartTemplate.php
+++ template/cartTemplate.php
@@ -3,6 +3,7 @@
         <h2><?php echo $templateParams["titolo_pagina"]; ?></h2>
         <?php endif;?>
 
+
             <section>
               <div class="col-md-4 order-md-2 mb-4" style="width: 60%">
                 <h4 class="d-flex justify-content-between align-items-center mb-3">
@@ -20,70 +21,68 @@
                                     <tr class="small text-uppercase">
                                         <th scope="col">Product</th>
                                         <th scope="col" width="120">Quantity</th>
-                                        <th scope="col" width="120">Price(EUR)</th>
+                                        <th scope="col" width="120">Price</th>
                                         <th scope="col" class="text-right d-none d-md-block"></th>
                                     </tr>
                                 </thead>
+                                <tbody>
                                   <?php foreach($templateParams["items"] as $item): ?>
-
-                                        <tbody>
-                                            <tr>
-                                                <td>
-                                                    <figure class="itemside align-items-center">
-                                                        <div class="aside"><img src="<?php echo $item["images"][0]; ?>" alt="PrimaryImage" style="height: 180px"></div>
-                                                        <figcaption class="info"> <a href="#" class="title text-dark" data-abc="true"><?php echo $item["name"]; ?></a>
-                                                            <p class="text-muted small">VENDOR: <?php echo $item["vendor"]; ?><br> CodArticle: MAXTRA</p>
-                                                        </figcaption>
-                                                    </figure>
-                                                </td>
-                                                <td> <select class="form-control">
-                                                        <option>1</option>
-                                                        <option>2</option>
-                                                        <option>3</option>
-                                                        <option>4</option>
-                                                    </select> </td>
-                                                <td>
-                                                    <div class="price-wrap"> <var class="price"><?php echo $item["price"]; ?></var></div>
-                                                </td>
-                                                <td class="text-right d-none d-md-block">
-                                                <button type="button" class="btn btn-outline-secondary">
-                                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-                                                    <path fill-rule="evenodd" d="M6.5 1a.5.5 0 0 0-.5.5v1h4v-1a.5.5 0 0 0-.5-.5h-3ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1H3.042l.846 10.58a1 1 0 0 0 .997.92h6.23a1 1 0 0 0 .997-.92l.846-10.58Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"></path>
-                                                  </svg>
-                                                <span class="visually-hidden">Button</span>
-                                                </button>
-                                                </td>
-                                              </tr>
-                                        </tbody>
-
+                                        
+                                    <tr>
+                                        <td>
+                                            <figure class="itemside align-items-center">
+                                                <div class="aside"><img src="<?php echo $item["images"][0]; ?>" alt="PrimaryImage" style="height: 180px"></div>
+                                                <figcaption class="info"> <a href="#" class="title text-dark" data-abc="true"><?php echo $item["name"]; ?></a>
+                                                    <p class="text-muted small">VENDOR: <?php echo $item["vendor"]; ?><br> CodArticle: MAXTRA</p>
+                                                </figcaption>
+                                            </figure>
+                                        </td>
+                                        <td> <select class="form-control">
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                            </select> </td>
+                                        <td>
+                                            <div class="price-wrap"> <var class="price"><?php echo $item["price"]; ?> €</var></div>
+                                        </td>
+                                        <td class="text-right d-none d-md-block">
+                                        <a type="button" class="btn btn-outline-secondary" id="remove-item" href="cartPage.php?delete=<?php echo $item["CodProdotto"]; ?>">
+                                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M6.5 1a.5.5 0 0 0-.5.5v1h4v-1a.5.5 0 0 0-.5-.5h-3ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1H3.042l.846 10.58a1 1 0 0 0 .997.92h6.23a1 1 0 0 0 .997-.92l.846-10.58Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"></path>
+                                          </svg>
+                                          <span class="visually-hidden">Button</span>
+                                        </a>
+                                        </td>
+                                    </tr> 
+                                        
                                   <?php endforeach; ?>
+                                </tbody>
                             </table>
                           </div>
                         </div> 
-                  <li class="list-group-item d-flex justify-content-between bg-light">
-                    <div class="text-success">
-                      <h6 class="my-0">Promo code</h6>
-                      <small>EXAMPLECODE</small>
-                    </div>
-                    <span class="text-success">-$5</span>
-                  </li>
-                  <li class="list-group-item d-flex justify-content-between">
-                    <span>Total (USD)</span>
-                    <strong><?php echo $templateParams["total"]; ?></strong>
-                  </li>
-                
-                <form class="card p-2">
-                  <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Promo code">
-                    <div class="input-group-append">
-                      <button type="submit" class="btn btn-secondary">Redeem</button>
-                    </div>
-                  </div>
-                </form>
-                </div>
+                        <li class="list-group-item d-flex justify-content-between bg-light visually-hidden">
+                          <div class="text-success">
+                            <h6 class="my-0">Promo code</h6>
+                            <small>EXAMPLECODE</small>
+                          </div>
+                          <span class="text-success">-$5</span>
+                        </li>
+                        <li class="list-group-item d-flex justify-content-between">
+                          <span>Total</span>
+                          <strong><?php echo $templateParams["total"]; ?> €</strong>
+                        </li>
+                        <form class="card p-2">
+                          <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Promo code">
+                            <div class="input-group-append">
+                              <button type="submit" class="btn btn-secondary">Redeem</button>
+                            </div>
+                          </div>
+                        </form>
+                        </div>
+                      </div>
                 </aside>
-
-
               </div>
 
 
@@ -188,10 +187,7 @@
           ================================================== -->
           <!-- Placed at the end of the document so the pages load faster -->
           <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-          <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-          <script src="../../assets/js/vendor/popper.min.js"></script>
-          <script src="../../dist/js/bootstrap.min.js"></script>
-          <script src="../../assets/js/vendor/holder.min.js"></script>
+
           <script>
             // Example starter JavaScript for disabling form submissions if there are invalid fields
             (function() {
