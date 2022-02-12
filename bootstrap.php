d5283db3149268c6420ffcbab3ac329c654a0dd3
Andrea Briglia <andrea.brigliadori3@studio.unibo.it>
2022-02-12 17:52:35 +0100

    minor fixes

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-//define("UPLOAD_DIR", "./images/");
 require_once("utils/functions.php");
 require_once("db/database.php");
 
@@ -8,6 +7,5 @@ $dbh = new DatabaseHelper("localhost", "root", "", "dsoundsystemLOGIC", 3306);
 if (isSet($_SESSION["Nickname"])) {
     $templateParams["notReadNotifies"] = $dbh->getNotReadNotifiesNumber($_SESSION["Nickname"]);
     $templateParams["notifies"] = $dbh->getNotifies($_SESSION["Nickname"]);
-    //print_r($templateParams["notifies"]);
 }
 ?> 
